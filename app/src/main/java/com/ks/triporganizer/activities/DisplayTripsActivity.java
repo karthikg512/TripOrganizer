ada7a6ee44984eeb02c11e28cf85191866381175
Karthik Gundimeda <kgundimeda@snapfish-llc.com>
2016-05-31 15:27:45 -0700

    list it out

--- app/src/main/java/com/ks/triporganizer/activities/DisplayTripsActivity.java
+++ app/src/main/java/com/ks/triporganizer/activities/DisplayTripsActivity.java
@@ -1,22 +1,33 @@
 package com.ks.triporganizer.activities;
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.ks.triporganizer.R;
 import com.ks.triporganizer.pojo.Trip;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class DisplayTripsActivity extends AppCompatActivity {
+public class DisplayTripsActivity extends ListActivity {
+
+    RestTemplate restTemplate = new RestTemplate();
+    ArrayList<String> listItems=new ArrayList<String>();
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,19 +37,24 @@ public class DisplayTripsActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String email = intent.getStringExtra(LoginActivity.EMAIL_ID);
 
+        adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+        setListAdapter(adapter);
+
         // get list of trips based on email
         new HttpRequestTask().execute();
 
     }
 
-    private class HttpRequestTask extends AsyncTask<Void, Void, List<Trip>> {
+    private class HttpRequestTask extends AsyncTask<Void, Void, Trip[]> {
         @Override
-        protected List<Trip> doInBackground(Void... params) {
+        protected Trip[] doInBackground(Void... params) {
             try {
                 final String url = "http://10.0.2.2:8080/trip-organizer/team/1";
-                RestTemplate restTemplate = new RestTemplate();
                 restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-                List<Trip> trips = restTemplate.getForObject(url, List.class);
+                ResponseEntity<Trip[]> responseEntity = restTemplate.getForEntity(url, Trip[].class);
+                Trip[] trips = responseEntity.getBody();
                 return trips;
             } catch (Exception e) {
                 Log.e("DisplayTripsActivity", e.getMessage(), e);
@@ -48,12 +64,17 @@ public class DisplayTripsActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(List<Trip> trips) {
-            TextView tripsView = (TextView) findViewById(R.id.trip_label);
-            if (trips == null)
-                tripsView.setText("null");
+        protected void onPostExecute(Trip[] trips) {
+
+            if (trips != null && trips.length > 0) {
+                for (Trip trip : trips) {
+                    listItems.add(trip.getTripName());
+                }
+                adapter.notifyDataSetChanged();
+            }
             else
-                tripsView.setText(trips.toString());
+                listItems.add("No Trips Yet!!");
         }
+
     }
 }
--- app/src/main/java/com/ks/triporganizer/pojo/Trip.java
+++ app/src/main/java/com/ks/triporganizer/pojo/Trip.java
@@ -1,16 +1,27 @@
 package com.ks.triporganizer.pojo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.Date;
 
 /**
  * Created by kgundimeda on 5/24/16.
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Trip {
 
     private Long id;
     private String tripName;
     private String purpose;
     private String destination;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getTripName() {
         return tripName;
